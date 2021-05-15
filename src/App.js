a47f3a02a85d49de00239f99d121993158beb2ee
Samrat Mallick <samrat.mallick@webskitters.com>
2021-05-15 10:43:07 +0530

    app Body

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import logo from "./asset/logo__home.jpg";
 
 function App() {
 	return (
 		<div className="App">
-			.div
-			<span> FISH FRY</span>
-			<div>Logo</div>
+			<div className="app__body">
+			<span className="app__heading"> FISH FRY</span>
+			<div ><img src={logo} alt="app__logo" className="app__logo"></img> </div>
+			<div className="app__button">
 			<Button variant="contained" color="primary" href="# ">Timer</Button>
 			<Button variant="contained" color="primary" href="# ">Score Card</Button>
+			</div>
+			</div>
 		</div>
 	);
 }
