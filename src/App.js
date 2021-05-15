5380b73d6791c83c23618f0e2284e00898c47417
Samrat Mallick <samrat.mallick@webskitters.com>
2021-05-15 09:47:21 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+	return (
+		<div className="App">
+			.div
+			<span> FISH FRY</span>
+			<div>Logo</div>
+			<Button variant="contained" color="primary" href="# ">Timer</Button>
+			<Button variant="contained" color="primary" href="# ">Score Card</Button>
+		</div>
+	);
+}
+
+export default App;
